4e995ec170da9d9b547493d33e08c9602fb8fdbc
dhruv <dhruvilkalathiya04@gmail.com>
2023-12-12 23:06:52 +0000

    My homeworkweek14 Orangehrm

---
+++ src/test/java/com/orangehrmlive/demo/testsuite/UsersTest.java
@@ -0,0 +1,91 @@
+package com.orangehrmlive.demo.testsuite;
+
+import com.orangehrmlive.demo.customelisteners.CustomListeners;
+import com.orangehrmlive.demo.pages.*;
+import com.orangehrmlive.demo.testbase.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import resources.testdata.TestData;
+@Listeners(CustomListeners.class)
+public class UsersTest extends BaseTest {
+    LoginPage loginPage;
+    HomePage homePage;
+    DashboardPage dashboardPage;
+    ViewSystemUsersPage viewSystemUsersPage;
+    AddUsersPage addUsersPage;
+
+    @BeforeMethod(alwaysRun = true)
+    public void inIt() {
+        loginPage = new LoginPage();
+        homePage = new HomePage();
+        dashboardPage = new DashboardPage();
+        viewSystemUsersPage = new ViewSystemUsersPage();
+        addUsersPage = new AddUsersPage();
+    }
+
+    @Test(groups = {"sanity", "smoke", "regression"})
+    public void adminShouldAddUserSuccessFully() {
+        loginPage.loginToTheApplication("Admin", "admin123");
+        homePage.clickOnAdminLink();
+
+        String expectedSystemUsersMessage = "System Users";
+        String actualSystemUsersMessage = viewSystemUsersPage.getSystemUsersText();
+        Assert.assertEquals(actualSystemUsersMessage, expectedSystemUsersMessage);
+
+        viewSystemUsersPage.clickOnAddButton();
+
+        String expectedAddUsersMessage = "Add User";
+        String actualAddUserMessage = addUsersPage.verifyAddUserText();
+        Assert.assertEquals(actualAddUserMessage, expectedAddUsersMessage);
+
+        addUsersPage.mouseHoverAndClickOnUserRoleDropDownInAddUser();
+        addUsersPage.mouseHoverAndCLickOnAdminOptionInUserRoleDropDownInAddUser();
+
+        addUsersPage.sendDataToEmployeeNameFieldInAddUser(" x x Zenaida Schultz");
+        addUsersPage.sendDataToUserNameFieldInAddUser("manish123");
+        addUsersPage.clickOnStatusDropDownMenuInAddUser();
+        addUsersPage.mouseHoverAndClickOnDisabledOptionInAddUser();
+        addUsersPage.sendDataToPasswordFieldInAddUser("manish123");
+        addUsersPage.sendDataToConfirmPasswordFieldInAddUser("manish123");
+        addUsersPage.clickOnSaveButtonInAddUser();
+
+        String expectedSuccessfullySavedMessage = "Successfully Saved";
+        String actualSuccessfullySavedMessage = addUsersPage.getSuccessfullySavedMessageInAddUser();
+        Assert.assertEquals(actualSuccessfullySavedMessage, expectedSuccessfullySavedMessage);
+    }
+
+    @Test(groups = {"smoke", "regression"})
+    public void searchTheUserCreatedAndVerifyIt() {
+        loginPage.loginToTheApplication("Admin", "admin123");
+        homePage.clickOnAdminLink();
+
+        String expectedSystemUsersMessage = "System Users";
+        String actualSystemUsersMessage = viewSystemUsersPage.getSystemUsersText();
+        Assert.assertEquals(actualSystemUsersMessage, expectedSystemUsersMessage);
+
+        viewSystemUsersPage.sendDataToUserNameFieldInSystemUsers("manish123");
+        viewSystemUsersPage.clickOnUserRoleDropDownInSystemUsers();
+        viewSystemUsersPage.mouseHoverAndClickOnAdminOptionInUserRoleDropDownInSystemUsers();
+        viewSystemUsersPage.clickOnStatusDropDownInSystemUsers();
+        viewSystemUsersPage.mouseHoverAndCLickOndDisableStatusOptionInSystemUsers();
+        viewSystemUsersPage.clickOnSearchButtonInSystemUsers();
+
+        String expectedUserName = "manish123";
+        String actualUserName = viewSystemUsersPage.getDataFromUserNameInRecord();
+        Assert.assertEquals(actualUserName, expectedUserName);
+    }
+
+    @Test(dataProvider = "userDetails", dataProviderClass = TestData.class, groups = {"regression"})
+    public void verifyThatAdminShouldDeleteTheUserSuccessFully(String uName, String userRole, String eName, String status){
+        loginPage.loginToTheApplication("Admin", "admin123");
+        homePage.clickOnAdminLink();
+
+        String expectedSystemUsersMessage = "System Users";
+        String actualSystemUsersMessage = viewSystemUsersPage.getSystemUsersText();
+        Assert.assertEquals(actualSystemUsersMessage, expectedSystemUsersMessage);
+
+        viewSystemUsersPage.searchUserDetails(uName, userRole, eName, status);
+    }
+}
